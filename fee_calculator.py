52dc77e75b19864c2e7c4f50f9d80247b3c82cef
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2021-08-28 00:07:45 -0300

    feat: implements strategy pattern

--- fee_calculator.py
+++ fee_calculator.py
@@ -4,17 +4,20 @@
 class FeeCalculator(object):
     """Calculator of fees"""
     
-    def calculate(self, budget):
+    def calculate(self, budget, tax_to_calculate):
         """Execute operations on a given budget"""
-        calculated_fee = budget.value * 0.015
+        calculated_fee = tax_to_calculate(budget.value)
         return calculated_fee
 
 if __name__ == '__main__':
 
     from budget import Budget
+    from patterns.strategy.taxes import ( calculate_tax_one, calculate_tax_two )
 
     my_budget = Budget(450)
     fee_calc = FeeCalculator()
 
-    result = fee_calc.calculate(my_budget)
-    print(result)
+    result_one = fee_calc.calculate(my_budget, calculate_tax_one)
+    print(result_one)
+    result_two = fee_calc.calculate(my_budget, calculate_tax_two)
+    print(result_two)
---
+++ patterns/strategy/taxes.py
@@ -0,0 +1,7 @@
+def calculate_tax_one(value):
+    """Calculate tax one"""
+    return value * 0.24
+
+def calculate_tax_two(value):
+    """Calculate tax two"""
+    return value * 0.163
