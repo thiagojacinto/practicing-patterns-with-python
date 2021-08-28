db9577a2a652938ff643a4c635ce76af02d2677b
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2021-08-28 11:18:26 -0300

    feat: budget now include Items

--- budget.py
+++ budget.py
@@ -3,8 +3,32 @@
 
 class Budget(object):
   
-    def __init__(self, value) -> None:
-        self.__value = value
+    def __init__(self) -> None:
+        # self.__value = value
+        self.__itens = []
+
+    @property
+    def value(self):
+        # return self.__value
+        total = 0.0
+        for item in self.__itens:
+            total += item.value
+        
+        return total
+    
+    def get_itens(self):
+        """Returns the itens included in the budget"""
+        return tuple(self.__itens)
+
+class Item():
+
+    def __init__(self, name, value) -> None:
+        self.__name = name
+        self.__value == value
+
+    @property
+    def name(self):
+        return self.__name
 
     @property
     def value(self):
