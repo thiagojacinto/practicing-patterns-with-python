1342b4f67b9465c51cbce7b3af187368ae4446f6
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2021-08-29 21:40:54 -0300

    feat: new more fees to calculate

--- src/features/fee_calculator.py
+++ src/features/fee_calculator.py
@@ -9,6 +9,36 @@ class FeeCalculator(object):
         calculated_fee = tax_to_calculate(budget.value)
         return calculated_fee
 
+# introducing new fees
+class NewFeeThree():
+    """Fee that uses progression to handle its impact"""
+    def __init__(self, budget) -> None:
+        self.__budget = budget
+
+    def calculate(self, value):
+        if value > 500:
+            return value * 0.1
+        else:
+            return value * 0.06
+
+class NewFeeFour():
+    """Fee that validate if one of the items is over some value"""
+    def __init__(self, budget) -> None:
+        self.__budget = budget
+
+    def calculate(self, value):
+        if value > 500 & self.has_one_item_over_100():
+            return value * 0.075
+        else:
+            return value * 0.045
+    
+    def has_one_item_over_100(self):
+        """Verify if one of the items of the budget has value over 100"""
+        for item in self.__budget.get_itens():
+            if item.value > 100: return True
+        return False
+
+
 if __name__ == '__main__':
 
     from src.budget import Budget, Item
@@ -26,3 +56,11 @@ if __name__ == '__main__':
     print(result_one)
     result_two = fee_calc.calculate(my_budget, calculate_tax_two)
     print(result_two)
+    
+    # applying new fees
+    fee_three = NewFeeThree(my_budget)
+    fee_four = NewFeeFour(my_budget)
+    result_three = fee_calc.calculate(my_budget, fee_three.calculate)
+    print(result_three)
+    result_four = fee_calc.calculate(my_budget, fee_four.calculate)
+    print(result_four)
